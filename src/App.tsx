16b71f8ae2d212c7fcc24ff9497c09f6fef8b4b5
dmitlewski <david.mitlewski@gmx.de>
2021-12-15 18:51:41 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import './App.css';
+
+// named import -> {}
+// from ./path , without extensions .ts, .tsx, .js, .jsx
+import {Welcome} from './Welcome'
+import {Game} from './Game'
+
+
+// export weil es im index.tsx importiert wird
+export function App() {
+  const[list , setList] = useState([]);
+  const[inputData , setInputData] = useState("test");
+  
+
+  function handleInputData(event : Event ){
+    setInputData(event);
+  }
+
+  function handleAddItem(){
+    const newList = [list , {inputData}]
+    setList(newList);
+    setInputData(""); //erase inputfield afterwards
+  }
+
+
+
+  return (
+    <div className="App">
+      <div className ="input">
+        <input type="text" value={inputData} onChange={handleInputData(event)}/>
+        <input type="button" value="ADD" onClick={handleAddItem()}/>
+      </div>
+
+      <div className="list">
+        {/* list be here */}
+      </div>
+    </div>
+  );
+}
---
+++ src/Welcome.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Welcome.css'
+
+
+// named export
+// export const Welcome = () =>
+//     <strong>Welcome to Rock Paper Scissors</strong>
+
+export function Welcome() {
+
+}
+
+
+
+
+
+
+
+
