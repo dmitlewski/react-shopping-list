37f506fbcebc2e99da8be82dea802c6b1836a931
dmitlewski <david.mitlewski@gmx.de>
2021-12-22 18:06:45 +0100

    x buttons

--- src/App.tsx
+++ src/App.tsx
@@ -11,34 +11,64 @@ export function App() {
 
   const[list , setList] = useState<String[]>([]);
   const[inputData , setInputData] = useState("");
-  const addText = "ADD";
-  const [fieldClicked , setFieldClicked] = useState(false);
+  const [textWasPutInBar , setTextWasPutInBar] = useState(false);
+  const [showDeleteButton , setShowDeleteButton] = useState(false);
+
   
   function getReady(){
     hideInitialText();
-    displayAddButton();
-
+    setShowDeleteButton(false);
   }
 
-  function handleInputData(event: React.ChangeEvent<HTMLInputElement>){ //abgefahrener Typ = INstanz
-    setInputData(event.target.value); //ist jetzt ein String
+  function handleInputData(event: React.ChangeEvent<HTMLInputElement>){ //abgefahrener Typ = Instanz
+    setInputData(event.target.value); //ist jetzt ein String und keine Instanz mehr - auf String runtergebrochen
+    showAddButton();
   }
 
   function handleAddItem(){
-
-    const newList = [...list , inputData] //array-spread
- 
+    const newList = [...list , inputData] //array-spread:bisherige Liste wird ausgepackt mit ... und die inputData angehängt
     setList(newList);
     setInputData(""); //erase inputfield afterwards
+    
   }
+
+  function removeItemFromList(index: number){
+    const newList = [];                 //newList wird angelegt, leer
+      for (let i = 0; i < list.length; i++){
+        if (index != i){ //wenn der index ungleich der Laufvariable, dann setze das Item auf die newList - d.h. alle nicht angeklickten Items werden wieder auf die newList gesetzt
+          newList.push(list[i]); //pushe das item mit dem Index i aus der list in die newList
+        }
+      }
+      setList(newList);     //mache newList zur list
+  }
+
+
+
 
   function hideInitialText(){
     setInputData(""); //erase initial message "type item here"
   }
 
-  function displayAddButton(){
-    setFieldClicked(true);
+  function showAddButton(){
+    setTextWasPutInBar(true);
   }
+
+  function handleShowDeleteButton(){
+    if(showDeleteButton === false){
+      setShowDeleteButton(true);
+    }
+    else if(showDeleteButton === true){
+      setShowDeleteButton(false);
+    }
+    
+  }
+
+
+
+//{}= Expression in JS
+//{}= Objekt im React-Syntax
+
+
   return (
     <div className="App">
       <p className = "welcomeTitle" >Welcome to the shopping list</p>
@@ -50,8 +80,8 @@ export function App() {
           value={inputData}
           placeholder={"type item here.."}
           className="textBar"
-          onClick={ function() { getReady() } }
-          onChange={ function blablaname(evt) { handleInputData(evt) } } //wenn onchange getriggert wird, dann dann wird eine function aufgeruf, die die Variable evt definiert und diese wird vom browser als ChangeEvent Objekt initilisiert
+          onClick={ function blablafooname () { getReady() } }
+          onChange={ function blablaname(evt) { handleInputData(evt) } } //wenn onchange getriggert wird, dann dann wird eine function erstellt, die die Variable evt definiert und diese wird vom browser als ChangeEvent Objekt initilisiert
           // onClick={getReady}
           // onChange={e => setInputData(e.target.value)}
         />
@@ -66,12 +96,39 @@ export function App() {
           })
         */}
 
-        {/* button wird erst angezeigt wenn man ins Feld klickt */}
-        {fieldClicked && <input type="button" value="ADD" className="button" onClick={handleAddItem}/>}
+        {/* button wird erst angezeigt wenn etwas eingegeben wurde ins Feld */}
+        {/*textWasPutInBar && <input type="button" value="ADD" className="button" onClick={handleAddItem}/>*/} {/* // ist das selbe wie der button unten bloß reactish */}
+        {textWasPutInBar && <button className="button" onClick={handleAddItem}>ADD</button>}
       </div>
+
+
+
+      {/* this is what we want */}
+      {/* <ul>
+        <li>foo</li>
+        <li>bar</li>
+        <li>baz</li>
+      </ul>*/}
+      {/*<ul className="list"> { ["asdf", "foo"] } </ul>*/}
+      {/*<ul className="list"> { [<li>asdf</li>, <li>foo</li>] } </ul>*/}
+      {/*<ul className="list">{list}</ul>*/}
       <div className="list">
-        {/* list be here */}
+        {
+          list.map(function transformStringToLi(item, index) {
+            return <div>
+            <span className="item" onClick={handleShowDeleteButton} >{item} </span>
+            {showDeleteButton && <button className="deleteButton" onClick={ function foo() {removeItemFromList(index)}} >X</button>}
+            </div>
+          })
+          // list.map(item => (
+          //   <li>
+          //     {item}
+          //     <button>X</button>
+          //   </li>
+          // ))
+        }
       </div>
+
     </div>
   );
 }
@@ -96,5 +153,8 @@ function myOtherFunction(d) {
 
 myFunction(8)
 myOtherFunction(8)
+
+{} = expression (wert) (innerhalb der JS Syntax)
+{} = erzeuge ein Objekt (innerhalb der React Syntax)
 
 */
